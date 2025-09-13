ec405ead1cfeb3c5295bd80064d48023289d048a
monikajha100 <monikailasharma@gmail.com>
2025-09-13 14:01:46 +0530

    Serve React build from backend

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const path = require('path');
+const app = express();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// React build folder ko serve karna
+app.use(express.static(path.join(__dirname, 'client_build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client_build', 'index.html'));
 });
 
-module.exports = router;
+// Port set karna (Heroku ke liye)
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
